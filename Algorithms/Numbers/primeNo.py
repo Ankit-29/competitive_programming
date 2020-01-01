5ab63ae02bbf6d9b53812ffe36a39a0ded2a4757
Ankit <iamankitkumar29@gmail.com>
2020-01-01 22:11:23 +0530

    Prime Number Generator Using Sieve of Eratosthenes

---
+++ Algorithms/Numbers/primeNo.py
@@ -0,0 +1,25 @@
+# Generate all prime Numbers upto a given Value N.
+# Algorihm Used :- Sieve of Eratosthenes
+
+def generatePrimeNos(N):
+    primes = [True]*(N+1)
+    primes[0]=False
+    primes[1]=False
+
+    for x in range(2,int(N**0.5)+1):
+        if(primes[x]):
+            y = 2
+            while(x*y<=N):
+                primes[x*y] = False
+                y +=1
+    
+    return primes
+
+
+def main():
+    N = int(input())
+    primeList = generatePrimeNos(N)
+    for x in range(N):
+        if primeList[x]:print(x)
+
+main()
