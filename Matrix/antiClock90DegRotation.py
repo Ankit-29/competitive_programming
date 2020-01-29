00f72661aa813e031ee912e31fbea9358bbbd00a
Ankit-29 <iamankitkumar29@gmail.com>
2020-01-30 00:20:44 +0530

    Anticlock 90 degree Inplace Rotation (Matrix)

--- Matrix/antiClock90DegRotation.py
+++ Matrix/antiClock90DegRotation.py
@@ -13,13 +13,13 @@ from typing import List
 
 def rotate90Deg(mat: List[List[int]]) -> List[List[int]]:
     size = len(mat)
-    for x in range(size-1,-1,-1):
-        col = x
-        row = size-x-1
-        while(row<size and col>=0):
-            mat[row][x],mat[size-x-1][col] = mat[size-x-1][col],mat[row][x]
-            col -= 1
-            row += 1
+    for x in range(size//2):
+        for y in range(x,size-x-1):
+            temp = mat[x][y]
+            mat[x][y] = mat[y][size-1-x]  
+            mat[y][size-1-x] = mat[size-1-x][size-1-y] 
+            mat[size-1-x][size-1-y] = mat[size-1-y][x] 
+            mat[size-1-y][x] = temp 
 
     return mat
 
