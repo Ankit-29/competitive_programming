bf33ef2c880f7b7ee469da0e147c61bc83153b28
Ankit-29 <iamankitkumar29@gmail.com>
2020-02-05 23:59:55 +0530

    Stone And Jewels (Hash Table)

--- HashTable/stoneAndJewels.py
+++ HashTable/stoneAndJewels.py
@@ -1,17 +1,20 @@
-
-
-
-def totalJewels(stones:str,jewels:str)-> int:
+'''
+You're given strings jewels representing the types of stones that are jewels, 
+and stones representing the stones you have. Each character in stones is a type 
+of stone you have. You want to know how many of the stones you have are also jewels.
+'''
+def totalJewels(stones: str, jewels: str) -> int:
     Hash = dict()
     count = 0
     for x in jewels:
         Hash[x] = True
+
     for x in stones:
         if(x in Hash):
             count += 1
-            
+
     return count
 
-jewels = "aA", 
+jewels = "aA"
 stones = "aAAbbbb"
-print(totalJewels(stones,jewels))
+print(totalJewels(stones, jewels))
