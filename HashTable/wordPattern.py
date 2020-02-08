bb2fad5aabc7c7963ac84c2b95efccd4911c7750
Ankit-29 <iamankitkumar29@gmail.com>
2020-02-08 23:56:46 +0530

    Word Pattern

---
+++ HashTable/wordPattern.py
@@ -0,0 +1,17 @@
+'''
+Given a pattern and a string str, find if str follows the same pattern.
+Here follow means a full match, such that there is a bijection between a 
+letter in pattern and a non-empty word in str.
+Input: pattern = "abba", str = "dog cat cat dog"
+Output: true
+Input: pattern = "aaaa", str = "dog cat cat dog"
+Output: false
+'''
+
+def wordPattern(pattern: str,S:str) -> bool:
+    pass
+
+
+pattern = "abba"
+S = "dog cat cat dog"
+print(wordPattern(pattern,S))
