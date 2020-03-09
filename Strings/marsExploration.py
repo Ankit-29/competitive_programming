90245527ce80a857eb03c64c60a88e267fe32cf8
Ankit-29 <iamankitkumar29@gmail.com>
2020-03-09 23:58:50 +0530

    Mars Exploration (Strings)

--- Strings/marsExploration.py
+++ Strings/marsExploration.py
@@ -1,3 +1,14 @@
+'''
+Sami's spaceship crashed on Mars! She sends a series of SOS messages to Earth for help.
+Letters in some of the SOS messages are altered by cosmic radiation during transmission. 
+Given the signal received by Earth as a string,determine how many letters of Sami's SOS 
+have been changed by radiation.
+For example, Earth receives SOSTOT. Sami's original message was SOSSOS.
+Two of the message characters were changed in transit.
+Print the number of letters in Sami's message that were altered by cosmic radiation.
+Input: SOSSPSSQSSOR
+Output: 3
+'''
 def marsExploration(s):
     i = 0
     count = 0
