8ad61e9e50b50e30913b9ee3d59690dc52fac009
Ankit-29 <iamankitkumar29@gmail.com>
2020-04-01 21:27:41 +0530

    Numbers Smaller Than Current Number (Numbers)

---
+++ Numbers/numbersSmallerThanCurrentNumber.py
@@ -0,0 +1,30 @@
+'''
+Given the array nums, for each nums[i] find out how many numbers in the array are smaller 
+than it. That is, for each nums[i] you have to count the number of valid j's such that 
+j != i and nums[j] < nums[i]. Return the answer in an array.
+Input: nums = [8,1,2,2,3]
+Output: [4,0,1,1,3]
+Explanation: 
+For nums[0]=8 there exist four smaller numbers than it (1, 2, 2 and 3). 
+For nums[1]=1 does not exist any smaller number than it.
+For nums[2]=2 there exist one smaller number than it (1). 
+For nums[3]=2 there exist one smaller number than it (1). 
+For nums[4]=3 there exist three smaller numbers than it (1, 2 and 2).
+Input: nums = [6,5,4,8]
+Output: [2,1,0,3]
+'''
+from typing import List
+def smallerNumbersThanCurrent(nums: List[int]) -> List[int]:
+        sortedVal = sorted(nums,reverse=True)
+        Hash = dict()
+        l = len(nums)
+        for idx in range(1,l):
+            if(sortedVal[idx]!=sortedVal[idx-1]):
+                Hash[sortedVal[idx-1]] = l-idx
+            
+        Hash[sortedVal[l-1]] = 0
+        
+        return [Hash[x] for x in nums]
+
+nums = [8,1,2,2,3]
+print(smallerNumbersThanCurrent(nums))
