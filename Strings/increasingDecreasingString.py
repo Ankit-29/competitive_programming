c5c7e2613ca40bf587aef0689018ae80c3c6342e
Ankit-29 <iamankitkumar29@gmail.com>
2020-05-22 23:07:51 +0530

    Increasing Decreasing String

--- Strings/increasingDecreasingString.py
+++ Strings/increasingDecreasingString.py
@@ -8,7 +8,7 @@ Given a string s. You should re-order the string using the following algorithm:
 - Repeat step 5 until you cannot pick more characters.
 - Repeat the steps from 1 to 6 until you pick all characters from s.
 - In each step, If the smallest or the largest character appears more than once you can choose any occurrence and 
-  append it to the result.
+  append it to the result
 
 Return the result string after sorting s with this algorithm.
 
