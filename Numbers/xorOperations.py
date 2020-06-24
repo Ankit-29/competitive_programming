6520c43c7d815de54eb52457704002cdf47acfb0
Ankit-29 <iamankitkumar29@gmail.com>
2020-06-24 23:59:27 +0530

    XorOperations (Number)

--- Numbers/xorOperations.py
+++ Numbers/xorOperations.py
@@ -1,6 +1,27 @@
-def xorOperation(self, n: int, start: int) -> int:
+'''
+Given an integer n and an integer start.
+
+Define an array nums where nums[i] = start + 2*i (0-indexed) and n == nums.length.
+
+Return the bitwise XOR of all elements of nums.
+
+Input: n = 5, start = 0
+Output: 8
+Explanation: Array nums is equal to [0, 2, 4, 6, 8] where (0 ^ 2 ^ 4 ^ 6 ^ 8) = 8.
+Where "^" corresponds to bitwise XOR operator.
+
+Input: n = 4, start = 3
+Output: 8
+Explanation: Array nums is equal to [3, 5, 7, 9] where (3 ^ 5 ^ 7 ^ 9) = 8.
+'''
+
+def xorOperation(n: int, start: int) -> int:
     res = start
     for i in range(1, n):
         temp = start + 2*i
         res ^= temp
     return res
+
+
+n = 1; start = 7
+print(xorOperation(n,start))
