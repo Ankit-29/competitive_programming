9f2ab61768aa13518be376b792c1101b235ce3bd
Ankit-29 <iamankitkumar29@gmail.com>
2020-09-02 22:18:18 +0530

    Sort By Parity (Numbers,Array)

---
+++ Numbers/sortByParity.py
@@ -0,0 +1,32 @@
+'''
+Given an array A of non-negative integers, return an array consisting of all the even elements of A, followed by all the odd elements of A.
+
+You may return any answer array that satisfies this condition.
+
+ 
+
+Example 1:
+
+Input: [3,1,2,4]
+Output: [2,4,3,1]
+The outputs [4,2,3,1], [2,4,1,3], and [4,2,1,3] would also be accepted.
+ 
+
+Note:
+
+1 <= A.length <= 5000
+0 <= A[i] <= 5000
+'''
+
+from typing import List
+
+def sortArrayByParity(A: List[int]) -> List[int]:
+    Hash = {0: [], 1: []}
+    for x in A:
+        Hash[x % 2].append(x)
+
+    return Hash[0]+Hash[1]
+
+
+A = [3,1,2,4]
+print(sortArrayByParity(A))
