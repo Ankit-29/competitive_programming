1d5aa6b3bfd54361fb9c1f70fe30400ca5c7bcec
Ankit-29 <iamankitkumar29@gmail.com>
2020-09-15 20:59:01 +0530

    Parking Tickets (Recursion)

--- Recursion/parkingTickets.py
+++ Recursion/parkingTickets.py
@@ -1,3 +1,8 @@
+'''
+For a online parking ticket app we need a function that can generate 3 letter unique code of format XXX 
+where X can be any alphabet (A-Z) or a number
+
+'''
 arr = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G',
        'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
@@ -14,6 +19,5 @@ def createCombos(newArr, arr, original, depth):
 
 
 combo = createCombos([], arr, arr, 2)
-with open("combo.txt","a+") as file:
-    for x in combo:
-        file.write(x+"\n")
+for x in combo:
+    print(x)
